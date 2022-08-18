75dbf5d9db76ff71926628640af0a6380ab1ca28
JISEONG <ketchupgirrrl@gmail.com>
2022-08-19 02:58:24 +0900

    TEST

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,15 +2,10 @@
 #include <string>
 
 
-
-
-
-
-
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_gnoes_chainsawman_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Commit TEST";
     return env->NewStringUTF(hello.c_str());
 }
